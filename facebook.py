27227bb949ceef81fe16acb4f90f9460f48561cc
bigerock1147 <80972711+bigerock1147@users.noreply.github.com>
2021-03-20 14:01:42 -0400

    commit message

---
+++ facebook.py
@@ -0,0 +1,6 @@
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName.lower() == "eric" and userPassword == "abc"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
